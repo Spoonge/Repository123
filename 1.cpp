b37fcb19b5c2f75060fc827cc13f1d4e41475ac5
Spoonge <79290066370@yandex.ru>
2019-06-11 03:59:06 +0300

    Update

--- 1.cpp
+++ 1.cpp
@@ -5,116 +5,60 @@
 using namespace std;
 
 int main()
-
 {
-
 	string s;
-
 	int d = 0, f = 0, g = 0, h = 0, j = 0, k = 0;
-
 	getline(cin, s);
-
 	s.length();
-
 	for (int x = 0; x < s.length(); x++)
-
 	{
-
 		{
-
 			if (s[x] == 'o')
-
 				d++;
-
 		}
-
 		while (s[x] == 'o')
-
 		{
-
 			s.erase(x, 1);
-
 			x--;
-
 		}
-
 		{
-
 			if (s[x] == 'e')
-
 				f++;
-
 		}
-
 		while (s[x] == 'e')
-
 		{
-
 			s.erase(x, 1);
-
 			x--;
-
 		}
-
 		{
-
 			if (s[x] == 'i')
-
 				g++;
-
 		}
-
 		while (s[x] == 'i')
-
 		{
-
 			s.erase(x, 1);
-
 			x--;
-
 		}
-
 		{
-
 			if (s[x] == 'a')
-
 				h++;
-
 		}
-
 		while (s[x] == 'a')
-
 		{
-
 			s.erase(x, 1);
-
 			x--;
-
 		}
-
 		{
-
 			if (s[x] == 'u')
-
 				j++;
-
 		}
-
 		while (s[x] == 'u')
 
 		{
-
 			s.erase(x, 1);
-
 			x--;
-
 		}
-
-
 	}
-
-	cout << "o = " << d << "; " << "e = " << f << "; " << "i = " << g << "; " << "a = " << h << "; " << "u = " << j << "; "<< endl;
-
+	cout << "o = " << d << "; " << "e = " << f << "; " << "i = " << g << "; " << "a = " << h << "; " << "u = " << j << "; " << endl;
 	return 0;
-
 }
